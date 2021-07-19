e79ad4b46127de4a49d0bdbf042daf8e60fee3bd
Morales Arriaga <c.morales.arriaga@accenture.com>
2021-07-19 13:09:51 -0500

    first commit

---
+++ libs/api-interface/src/lib/api-interface.ts
@@ -0,0 +1,11 @@
+export interface ApiResponse {
+  message: string;
+}
+
+export const API_URL = '/api';
+
+/*
+export function apiInterface(): string {
+  return 'api-interface';
+}
+*/
---
+++ libs/ui-header/src/lib/ui-header.tsx
@@ -0,0 +1,14 @@
+import './ui-header.module.scss';
+
+/* eslint-disable-next-line */
+export interface UiHeaderProps {}
+
+export function UiHeader(props: UiHeaderProps) {
+  return (
+    <div>
+      <h1>Welcome to ui-header!</h1>
+    </div>
+  );
+}
+
+export default UiHeader;
